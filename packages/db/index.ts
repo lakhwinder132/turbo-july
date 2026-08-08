49ea4babdbcd1799f1ec9131eb7f859f7de9f2ad
Shelly <git remote add origin https://github.com/lakhwinder132/automatic-meme>
2026-08-08 22:19:14 +0530

    e

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -14,6 +14,6 @@ import { PrismaPg } from "@prisma/adapter-pg";
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
 const prismaClient = new PrismaClient({ adapter });
 
-console.log("DB package sees:", process.env["DATABASE_URL"]);
+// console.log("DB package sees:", process.env["DATABASE_URL"]);
 
 export default prismaClient;
